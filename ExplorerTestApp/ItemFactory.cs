0b018523ec346d4a0defc1ab8ee02d209e8e2cfc
MartinVoehringer <martin.voehringer@student.reutlingen-university.de>
2016-09-04 11:24:49 +0200

    Added default folder Icon and changed default view location. Also added a comment about modifying the default context menu.

--- ExplorerTestApp/ItemFactory.cs
+++ ExplorerTestApp/ItemFactory.cs
@@ -1,35 +1,49 @@
-﻿/**
+﻿using ExplorerTreeView;
+
+/**
  * Created by SharpDevelop.
  * User: omggomb
  * Date: 15.06.2014
  * Time: 11:29
  */
+
 using System;
-using ExplorerTreeView;
+using System.Windows;
+using System.Windows.Media.Imaging;
 
 namespace ExplorerTestApp
 {
-	/// <summary>
-	/// Description of ItemFactory.
-	/// </summary>
-	public class ItemFactory : ExplorerTreeView.TreeItemFactory
-	{
-		public ItemFactory()
-		{
-		}
+    /// <summary>
+    /// Description of ItemFactory.
+    /// </summary>
+    public class ItemFactory : ExplorerTreeView.TreeItemFactory
+    {
+        public ItemFactory()
+        {
+        }
 
-		#region TreeItemFactory implementation
+        #region TreeItemFactory implementation
 
-		public CustomTreeItem CreateCustomTreeItemInstance()
-		{
-			return new CustomTreeItem();
-		}
+        public CustomTreeItem CreateCustomTreeItemInstance()
+        {
+            // Using TreeViewItem supplied by the control.
+            // If you want to change the context menu, use ExplorerTreeView.GloablContextMenu
+            return new CustomTreeItem();
+        }
 
-		public System.Windows.Controls.Image CreateFolderIconImage(CustomTreeItem itemThatIsUsed)
-		{
-			return null;
-		}
+        public System.Windows.Controls.Image CreateFolderIconImage(CustomTreeItem itemThatIsUsed)
+        {
+            var image = new System.Windows.Controls.Image();
 
-		#endregion
-	}
+            var src = System.Windows.Interop.Imaging.CreateBitmapSourceFromHIcon(Properties.Resource1.SimpleFolderIcon.Handle,
+                                                                             Int32Rect.Empty,
+                                                                               BitmapSizeOptions.FromEmptyOptions());
+
+            image.Stretch = System.Windows.Media.Stretch.Fill;
+            image.Source = src;
+            return image;
+        }
+
+        #endregion TreeItemFactory implementation
+    }
 }
